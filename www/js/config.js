1721e17a91de85f4baca481b4ebc85679ac49382
unknown <bjayteh@gmail.com>
2019-06-06 15:56:18 +0000

    Bug fixes

---
+++ www/js/config.js
@@ -0,0 +1,27 @@
+
+var krms_config ={			
+	'ApiUrl':"http://prontogambia.com/mobileapp/api",		
+	'DialogDefaultTitle':"Chopsa",	
+	'pushNotificationSenderid':"152989422125",	
+	//'facebookAppId':"574771432913114", // 
+	//'facebookAppId':'389167844871484',
+	//'facebookAppId':'1792346294404924',
+	'facebookAppId':'',
+	'APIHasKey':""
+};
+
+
+/*new
+AIzaSyDW44BFxQQGe1EocP1NuHhAs7XaX1WinG0
+809241148941*/
+
+/*old api key
+483603428477
+AIzaSyBFQ9ErCsZgTvGbMC4mIBtS-HhZ9Jf-Mws
+*/
+
+
+/*NEW PUSH API KEY
+152989422125
+AIzaSyBghbC8Iwwij1OjNO6NpUYIy_GOBJ_J4f0
+*/
